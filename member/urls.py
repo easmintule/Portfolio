e86faa1c7a51665a5fbbdbee9fe6e42743e252da
easmintule <suraiaeasmin2526@gmail.com>
2025-09-24 22:49:04 +0600

    created a model

--- member/urls.py
+++ member/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('members/', views.members, name='members'),
-     path('accounts/', views.members, name='accounts'),
+    path('members/', views.members, name='members'),
+     #path('accounts/', views.members, name='accounts'),
      path('logins/', views.login, name='login'),
 
 

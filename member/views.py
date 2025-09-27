0d23ed32387fc946d7f67cc38d82ac7c2961ff64
easmintule <suraiaeasmin2526@gmail.com>
2025-09-28 00:15:44 +0600

    modify view

--- member/views.py
+++ member/views.py
@@ -2,10 +2,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymember = Member.objects.all().values()
+    template = loader.get_template('all_member.html')
+    context = {'mymember' : mymember}
+    return HttpResponse(template.render(context,request))
+
 
 def login(request):
     return HttpResponse("Welcome to login")

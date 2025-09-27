ff231ee52bc99e9d9b499cf5ac7e32cdc4d7f86d
easmintule <suraiaeasmin2526@gmail.com>
2025-09-28 00:02:18 +0600

    working on dynamic html

--- member/models.py
+++ member/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length =255)
     last_name = models.CharField(max_length =255)
-
+    email = models.EmailField(max_length =260,default='default@example.com')

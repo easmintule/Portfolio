81cd5883f72cd9e7838fffbe235240be4a18fdc8
easmintule <suraiaeasmin2526@gmail.com>
2025-10-06 21:06:16 +0600

    creating a new template

--- member/views.py
+++ member/views.py
@@ -7,7 +7,7 @@ from .models import Member
 def members(request):
     mymember = Member.objects.all().values()
     template = loader.get_template('all_member.html')
-    context = {'mymember' : mymember}
+    context = {'mymembers' : mymember}
     return HttpResponse(template.render(context,request))
 
 

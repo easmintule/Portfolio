594035dc99d295c1ef07b560f852262693d0ec8c
easmintule <suraiaeasmin2526@gmail.com>
2025-10-06 21:43:45 +0600

    Details Template

--- member/urls.py
+++ member/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details,name='details'),
      #path('accounts/', views.members, name='accounts'),
      path('logins/', views.login, name='login'),
 

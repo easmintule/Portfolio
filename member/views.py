733e3035b390891be4b72b31177827e80c6f18a9
easmintule <suraiaeasmin2526@gmail.com>
2025-10-06 21:59:41 +0600

    Details Template

--- member/views.py
+++ member/views.py
@@ -12,7 +12,7 @@ def members(request):
 
 
 def details(request, id):
-    mymember = Member.objects.get(id.id)
+    mymember = Member.objects.get(id=id)
     template = loader.get_template('details.html')
     context = {'mymember': mymember,}
     return HttpResponse(template.render(context,request))
